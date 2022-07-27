a294eddd06e70da4fbb731bf6b5607e4d159e7c1
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-07-27 15:38:46 -0500

    Fixed output box added JS variables

---
+++ script.js
@@ -0,0 +1,17 @@
+const buttonOne = document.querySelector("#buttonOne");
+const buttonTwo= document.querySelector("#buttonTwo");
+const buttonThree = document.querySelector("#buttonThree");
+const buttonFour = document.querySelector("#buttonFour");
+const buttonFive = document.querySelector("#buttonFive");
+const buttonSix = document.querySelector("#buttonSix");
+const buttonSeven = document.querySelector("#buttonSeven");
+const buttonEight = document.querySelector("#buttonEight");
+const buttonNine = document.querySelector("#buttonNine");
+const buttonZero = document.querySelector("#buttonZero");
+const buttonAdd = document.querySelector("#buttonAdd");
+const buttonSubtract = document.querySelector("#buttonSubtract");
+const buttonMultiply = document.querySelector("#buttonMultiply");
+const buttonDivide = document.querySelector("#buttonDivide");
+const output = document.querySelector("output")
+
+output.value= 3;
