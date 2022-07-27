b19504784d370127966cfc5ced7f3b85fcc25377
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-07-27 17:09:01 -0500

    Rough outline of thought process commented

--- script.js
+++ script.js
@@ -16,38 +16,39 @@ const buttonEquals =document.querySelector("#buttonEquals")
 const output = document.querySelector("output")
 
 
-let primaryValue = 0;
-let secondaryValue =0;
+let primaryValue = "";
+let secondaryValue = "";
 let isAddition = false;
 let isSubtraction = false;
 let isMultiplication = false;
 let isDivision = false;
 
+//Create a "stored data" output, so on secondary value area have another output box on top that displays Primary Value 
 
 
 buttonOne.addEventListener("click", () => {
     if (primaryValue >= 0 && isAddition === true || isSubtraction === true || isMultiplication === true || isDivision === true) {
-        secondaryValue = secondaryValue + Number(buttonOne.value);
+        secondaryValue = secondaryValue + buttonOne.value;
         output.value = secondaryValue;
     } else {
-        primaryValue = primaryValue + Number(buttonOne.value);
+        primaryValue = primaryValue + buttonOne.value;
         output.value= primaryValue;
     }
 })
+//Create button click events that set the subsequent operators flag to true.
 
-buttonTwo.addEventListener("click", () => {
-    if (primaryValue >= 0 && isAddition === true || isSubtraction === true || isMultiplication === true || isDivision === true) {
-        secondaryValue = buttonTwo.value;
-        output.value = buttonTwo.value;
-    } else {
-        primaryValue = buttonTwo.value;
-        output.value= buttonTwo.value;
-    }
-})
+//CLICK ON MULTI if (isAddition=false && isSubtraction = false && isDivision = false ){ isMultiplication = true}
 
 
 
 
+//create an if statement that will perform the operation according to which flag is set to true.
+//if (isAddition = true && secondaryValue > 0) { output.value = Number(primaryValue) + Number(secondayValue)}
 buttonEquals.addEventListener("click", () =>{
+    if (primaryValue > 0) {
+        console.log(Number(primaryValue) + Number(primaryValue))
+    }
     output.value = primaryValue;
 })
+
+//Create a Clear option that clears addition etx flags and sets the values back to ""
